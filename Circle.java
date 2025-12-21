b79ca27a864ac9bb3816fd8d7364da6e27022315
karthika3010 <maheswarikarthika30@gmail.com>
2025-12-20 19:43:12 -0800

    added circle file

---
+++ Circle.java
@@ -0,0 +1,21 @@
+class Circle
+{
+	public static void main(String[] args)
+	{
+		double PI=3.14;
+		double radious=6;
+		double area=PI*radious*radious;
+		System.out.println("area of circle:"+area);
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+}
