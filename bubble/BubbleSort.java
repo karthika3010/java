a49f36c61d4a55032b324a1b3d6220fba6f1b957
karthika3010 <maheswarikarthika30@gmail.com>
2026-01-22 23:10:09 -0800

    22/01/26 hw

---
+++ bubble/BubbleSort.java
@@ -0,0 +1,28 @@
+class BubbleSort
+{
+	public static void main(String[] args)
+	{
+		int arr[]={6,5,1,4,2,8,3};
+		int noofpasses=0;
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+			
+		    noofpasses++;
+		}
+		
+		System.out.print("Sorted array: ");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+		
+		System.out.print("number of passes: "+noofpasses);
+	}
+}
